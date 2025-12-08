be3811558e34e1f0b404cb0ae1bcf5bfa1d2a3a4
Kaio Cezar <kadisk.shark@gmail.com>
2025-12-08 13:10:13 -0300

    user-space.webapp

---
+++ VirtualDesk.Module/Webservices.layer/user-space.webservice/src/Controllers/UserSpace.controller.js
@@ -0,0 +1,16 @@
+const UserSpaceController = (params) =>{
+
+
+    const ListMyApps = (userId) => {
+
+    }
+
+    const controllerServiceObject = {
+        controllerName   : "UserSpaceController",
+        ListMyApps
+    }
+
+    return Object.freeze(controllerServiceObject)
+}
+
+module.exports = UserSpaceController
