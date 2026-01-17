90326c235391d530db20b18f8ba366fe58b53d14
Kaio Cezar <kadisk.shark@gmail.com>
2026-01-16 22:14:09 -0300

    ajuste de metadata

--- VirtualDesk.Module/PlatformApplications.layer/identity-and-access-management.app/src/Controllers/OrganizationManagement.controller.js
+++ VirtualDesk.Module/PlatformApplications.layer/identity-and-access-management.app/src/Controllers/OrganizationManagement.controller.js
@@ -3,12 +3,12 @@ const OrganizationManagementController = (params) => {
     const { organizationManagerService } = params
 
     const controllerServiceObject = {
-        controllerName           : "OrganizationManagementController",
-        CreateOrganization       : organizationManagerService.CreateOrganization,
-        ListOrganizations        : organizationManagerService.ListOrganizations,
-        GetOrganization          : organizationManagerService.GetOrganization,
-        UpdateOrganizationName   : organizationManagerService.UpdateOrganizationName,
-        DeleteOrganization       : organizationManagerService.DeleteOrganization,
+        controllerName         : "OrganizationManagementController",
+        CreateOrganization     : organizationManagerService.CreateOrganization,
+        ListOrganizations      : organizationManagerService.ListOrganizations,
+        GetOrganization        : organizationManagerService.GetOrganization,
+        UpdateOrganizationName : organizationManagerService.UpdateOrganizationName,
+        DeleteOrganization     : organizationManagerService.DeleteOrganization,
     }
     
     return Object.freeze(controllerServiceObject)
