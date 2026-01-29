8732ead36608fc49d496ca6f078dfe99ce15a527
Kaio Cezar <kadisk.shark@gmail.com>
2026-01-29 13:49:02 -0300

    tabs

--- Platform.Module/PanelApplications.layer/ServiceOrchestratorPanel.group/service-orchestrator-panel.webgui/src/Containers/MyServices.container/MyServices.container.tsx
+++ Platform.Module/PanelApplications.layer/ServiceOrchestratorPanel.group/service-orchestrator-panel.webgui/src/Containers/MyServices.container/MyServices.container.tsx
@@ -1,7 +1,7 @@
-import * as React             from "react"
-import {useEffect, useState, useRef}  from "react"
-import { connect }            from "react-redux"
-import { bindActionCreators } from "redux"
+import * as React                    from "react"
+import {useEffect, useState, useRef} from "react"
+import { connect }                   from "react-redux"
+import { bindActionCreators }        from "redux"
 
 import GetAPI from "../../Utils/GetAPI"
 
@@ -12,8 +12,9 @@ import NamespaceAndRepositoryManagerModal from "./NamespaceAndRepositoryManager.
 import ImportingModal from "./Importing.modal"
 import ServiceDetailsOffcanvas from "./ServiceDetails.offcanvas"
 
-
 import useWebSocket from "../../Hooks/useWebSocket"
+
+import ServiceCard from "./ServiceCard"
 
 const DEFAULT_MODE              = Symbol()
 const IMPORT_SELECT_MODE        = Symbol()
@@ -23,12 +24,8 @@ const LOADING_MODE              = Symbol()
 const SERVICE_PROVISIONING_MODE = Symbol()
 const REPOSITORIES_MANAGER_MODE = Symbol()
 
-const START_ICON     = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" /></svg>
-const RESTART_ICON   = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
-const STOP_ICON      = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-player-stop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 5m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" /></svg>
-const SETTINGS_ICON  = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-world-cog"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 12a9 9 0 1 0 -8.979 9" /><path d="M3.6 9h16.8" /><path d="M3.6 15h8.9" /><path d="M11.5 3a17 17 0 0 0 0 18" /><path d="M12.5 3a16.992 16.992 0 0 1 2.522 10.376" /><path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M19.001 15.5v1.5" /><path d="M19.001 21v1.5" /><path d="M22.032 17.25l-1.299 .75" /><path d="M17.27 20l-1.3 .75" /><path d="M15.97 17.25l1.3 .75" /><path d="M20.733 20l1.3 .75" /></svg>
-const WORLD_OFF_ICON = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-world-off"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5.657 5.615a9 9 0 1 0 12.717 12.739m1.672 -2.322a9 9 0 0 0 -12.066 -12.084" /><path d="M3.6 9h5.4m4 0h7.4" /><path d="M3.6 15h11.4m4 0h1.4" /><path d="M11.5 3a17.001 17.001 0 0 0 -1.493 3.022m-.847 3.145c-.68 4.027 .1 8.244 2.34 11.833" /><path d="M12.5 3a16.982 16.982 0 0 1 2.549 8.005m-.207 3.818a16.979 16.979 0 0 1 -2.342 6.177" /><path d="M3 3l18 18" /></svg>
-
+const OVERVIEW_TAB  = Symbol()
+const DETAILS_TAB   = Symbol()
 
 const GetStatusBadgeClasses = (status: string) => {
     switch (status) {
@@ -56,6 +53,38 @@ const GetStatusBadgeClasses = (status: string) => {
     }
 }
 
+const ServiceOverview = ({
+    servicesList,
+    onSelectService,
+    onStartService,
+    onStopService
+}) => {
+    return <div className="py-4">
+                {
+                    servicesList 
+                    && <div className="row">
+                            {
+                                servicesList.map((provisionedService, index) => (
+                                    <div key={index} className="col-md-4">
+                                        <ServiceCard 
+                                            serviceId={provisionedService.serviceId}
+                                            serviceIdSelected={servicesList}
+                                            status={provisionedService.status}
+                                            serviceName={provisionedService.serviceName}
+                                            originRepositoryNamespace={provisionedService.originRepositoryNamespace}
+                                            originPackageName={provisionedService.originPackageName}
+                                            originPackageType={provisionedService.originPackageType}
+                                            onSelectService={onSelectService}
+                                            onStartService={onStartService}
+                                            onStopService={onStopService}
+                                        />
+                                    </div>))
+                            }
+                        </div>
+                }
+            </div>
+}
+
 const MyServicesContainer = ({
     HTTPServerManager
 }) => {
@@ -65,6 +94,8 @@ const MyServicesContainer = ({
     const [ provisionedServicesList, setProvisionedServicesList ] = useState([])
 
     const [ serviceIdSelected, setServiceIdSelected] = useState()
+
+    const [ tabsCurrent, setTabsCurrent ] = useState<any>(OVERVIEW_TAB)
 
     const provisionedServicesListRef = useRef(provisionedServicesList)
 
@@ -186,65 +217,29 @@ const MyServicesContainer = ({
                             </div>
                         }
                     </div>
-                    <div className="py-4">
-                        {
-                            provisionedServicesList 
-                            && <div className="row">
-                                    {
-                                        provisionedServicesList.map((provisionedService, index) => (
-                                            <div key={index} className="col-md-4">
-                                                <div className={`card card-link mb-3 hover-shadow-lg cursor-pointer ${serviceIdSelected === provisionedService.serviceId ? "border-dark" : ""}`}
-                                                    onClick={() => handleSelectService(provisionedService.serviceId)}>
-                                                    <div className="card-header py-2">
-                                                        <div>
-                                                            <h4 className="card-title"><span className={`${GetStatusBadgeClasses(provisionedService.status)} me-2`}>{provisionedService.status}</span><strong>{provisionedService.serviceName}</strong></h4>
-                                                            <p className="card-subtitle">{provisionedService.originRepositoryNamespace}/{provisionedService.originPackageName}/{provisionedService.originPackageType}</p>
-                                                        </div>
-                                                    </div>
-                                                    {
-                                                            ( provisionedService.status === "STOPPING" || provisionedService.status === "STARTING" || provisionedService.status === "RESTARTING")
-                                                            &&<div className="card-body">
-                                                                    <div className="progress progress-sm">
-                                                                        <div className="progress-bar progress-bar-indeterminate"></div>
-                                                                    </div>
-                                                                </div>
-                                                    }
-                                                    <div className="card-footer bg-blue-lt">
-                                                        <div className="btn-list justify-content-end">
-                                                            {
-                                                                ( provisionedService.status === "STOPPED"
-                                                                || provisionedService.status === "TERMINATED" )
-                                                                && <button className="btn btn-primary" onClick={() => handleStartService(provisionedService.serviceId)}>
-                                                                        {START_ICON}start
-                                                                    </button>
-                                                            }
-                                                            {
-                                                                provisionedService.status === "RUNNING"
-                                                                && <>
-                                                                        <button className="btn btn-danger" onClick={() => handleStopService(provisionedService.serviceId)}>
-                                                                            {STOP_ICON}stop
-                                                                        </button>
-                                                                    </>
-                                                            }
-                                                            <a className="btn btn-sencondary" href={`#/my-services/service-settings/${provisionedService.serviceId}`}>
-                                                                {SETTINGS_ICON}settings
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>))
-                                    }
-                                </div>
-                        }
-                    </div>
+                    <ul className="nav nav-bordered mb-4">
+                        <li className="nav-item cursor-pointer">
+                            <a className={`nav-link ${tabsCurrent === OVERVIEW_TAB ? "active" : ""}`} onClick={() => setTabsCurrent(OVERVIEW_TAB)}>Overview</a>
+                        </li>
+                        <li className="nav-item cursor-pointer">
+                            <a className={`nav-link ${tabsCurrent === DETAILS_TAB ? "active" : ""}`} onClick={() => setTabsCurrent(DETAILS_TAB)}>Details</a>
+                        </li>
+                    </ul>
+                    
+
+                    {
+                        tabsCurrent === OVERVIEW_TAB
+                        && <ServiceOverview 
+                                servicesList={provisionedServicesList}
+                                onSelectService={handleSelectService}
+                                onStartService={handleStartService}
+                                onStopService={handleStopService}/>
+                    }
                 </div>
-
-
                 {
                     interfaceModeType === IMPORT_SELECT_MODE
                     && <ImportRepositoryModal onImport={handleImportingMode} onClose={() => changeMode(DEFAULT_MODE)} />
                 }
-
                 {
                     interfaceModeType === SERVICE_PROVISIONING_MODE
                     && <ServiceProvisioningModal onClose={() => changeMode(DEFAULT_MODE)} />
@@ -253,17 +248,14 @@ const MyServicesContainer = ({
                     interfaceModeType === REPOSITORIES_MANAGER_MODE
                     && <NamespaceAndRepositoryManagerModal onImportNew={() => changeMode(IMPORT_SELECT_MODE)} onClose={() => changeMode(DEFAULT_MODE)} />
                 }
-
                 {
                    interfaceModeType === NO_REPOSITORIES_MODE
                    && <WelcomeMyServices onImportNew={() => changeMode(IMPORT_SELECT_MODE)} onUseFromMyWorkspace={handleUseFromMyWorkspace}/>
                 }
-
                 {
                     interfaceModeType === IMPORTING_MODE 
                     && <ImportingModal importData={importDataCurrent} onFinishedImport={handleFinishedImportModal}/>
                 }
-
                 {
                     interfaceModeType === LOADING_MODE
                     && <div className="text-center py-4">
