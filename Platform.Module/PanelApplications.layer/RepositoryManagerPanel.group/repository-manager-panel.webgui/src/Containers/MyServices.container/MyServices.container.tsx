e2cc848af742d67a518dbd8f3a0e1528d8755560
Kaio Cezar <kadisk.shark@gmail.com>
2026-03-19 14:02:25 -0300

    preparação Repository Manager Panel

--- Platform.Module/PanelApplications.layer/RepositoryManagerPanel.group/repository-manager-panel.webgui/src/Containers/MyServices.container/MyServices.container.tsx
+++ Platform.Module/PanelApplications.layer/RepositoryManagerPanel.group/repository-manager-panel.webgui/src/Containers/MyServices.container/MyServices.container.tsx
@@ -13,15 +13,8 @@ import GetAPI from "../../Utils/GetAPI"
 
 import WelcomeMyServices from "./WelcomeMyServices"
 import ImportRepositoryModal from "./ImportRepository.modal"
-import ServiceProvisioningModal from "./ServiceProvisioning.modal"
 import NamespaceAndRepositoryManagerModal from "./NamespaceAndRepositoryManager.modal"
 import ImportingModal from "./Importing.modal"
-import ServiceDetailsOffcanvas from "./ServiceDetails.offcanvas"
-
-import useWebSocket from "../../Hooks/useWebSocket"
-
-import ServiceOverview from "./ServiceOverview.view"
-import ServiceDetails from "./ServiceDetails.view"
 
 import QueryParamsActionsCreator    from "../../Actions/QueryParams.actionsCreator"
 
@@ -30,7 +23,7 @@ const IMPORT_SELECT_MODE = Symbol()
 const IMPORTING_MODE = Symbol()
 const NO_REPOSITORIES_MODE = Symbol()
 const LOADING_MODE = Symbol()
-const SERVICE_PROVISIONING_MODE = Symbol()
+
 const REPOSITORIES_MANAGER_MODE = Symbol()
 
 const OVERVIEW_TAB = Symbol()
@@ -50,8 +43,6 @@ const MyServicesContainer = ({
     const [importDataCurrent, setImportDataCurrent] = useState<{ repositoryNamespace: string, sourceCodeURL: string }>()
     const [interfaceModeType, changeMode] = useState<any>(LOADING_MODE)
     const [provisionedServicesList, setProvisionedServicesList] = useState([])
-
-    const [serviceIdSelected, setServiceIdSelected] = useState()
 
     const [tabsCurrent, setTabsCurrent] = useState<any>()
 
@@ -109,24 +100,6 @@ const MyServicesContainer = ({
             serverManagerInformation: HTTPServerManager
         })
 
-    const updateServiceStatus = ({ serviceId, status }) => {
-        const listWithStatusUpdated = provisionedServicesListRef.current
-            .map((serviceData) => {
-                if (parseInt(serviceId) === serviceData.serviceId) {
-                    serviceData.status = status
-                }
-                return serviceData
-            })
-        setProvisionedServicesList(listWithStatusUpdated)
-    }
-
-    useWebSocket({
-        socket: _MyServicesAPI().ServicesStatusChange,
-        onMessage: updateServiceStatus,
-        onConnection: () => { },
-        onDisconnection: () => { }
-    })
-
     const fetchMyServicesStatus = async () => {
         const api = _RepositoryServiceAPI()
         const response = await api.CheckRepositoryImported()
@@ -135,16 +108,6 @@ const MyServicesContainer = ({
         } else if (response.data === "NO_REPOSITORIES") {
             changeMode(NO_REPOSITORIES_MODE)
         }
-    }
-
-    const startService = async (serviceId) => {
-        const api = _MyServicesAPI()
-        await api.StartService({ serviceId })
-    }
-
-    const stopService = async (serviceId) => {
-        const api = _MyServicesAPI()
-        await api.StopService({ serviceId })
     }
 
     const fetchProvisionedServices = async () => {
@@ -157,10 +120,6 @@ const MyServicesContainer = ({
         console.log("== handleUseFromMyWorkspace")
     }
 
-    const handleStartService = (serviceId) => startService(serviceId)
-
-    const handleStopService = (serviceId) => stopService(serviceId)
-
     const handleImportingMode = (importData) => {
         setImportDataCurrent(importData)
         changeMode(IMPORTING_MODE)
@@ -168,29 +127,13 @@ const MyServicesContainer = ({
 
     const handleFinishedImportModal = () => changeMode(LOADING_MODE)
 
-    const handleSelectService = (serviceId) => setServiceIdSelected(serviceId)
-
-    const handleCloseServiceDetails = () => setServiceIdSelected(undefined)
-
     return <>
-        {
-            serviceIdSelected !== undefined
-            && <ServiceDetailsOffcanvas
-                serviceId={serviceIdSelected}
-                onClose={() => handleCloseServiceDetails()} />
-        }
         <div className="container-xl">
             <div className="row g-2 align-items-center">
                 {
                     interfaceModeType === DEFAULT_MODE
                     && <div className="col-auto ms-auto d-print-none">
                         <div className="btn-list">
-                            <span className="d-none d-sm-inline">
-                                <button className="btn btn-cyan" onClick={() => changeMode(SERVICE_PROVISIONING_MODE)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-world-upload"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M21 12a9 9 0 1 0 -9 9" /><path d="M3.6 9h16.8" /><path d="M3.6 15h8.4" /><path d="M11.578 3a17 17 0 0 0 0 18" /><path d="M12.5 3c1.719 2.755 2.5 5.876 2.5 9" /><path d="M18 21v-7m3 3l-3 -3l-3 3" /></svg>
-                                    Service provisioning
-                                </button>
-                            </span>
                             <button className="btn btn-outline-cyan" onClick={() => changeMode(REPOSITORIES_MANAGER_MODE)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-folders"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 3h3l2 2h5a2 2 0 0 1 2 2v7a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" /><path d="M17 16v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h2" /></svg>
                                 Repository manager
@@ -207,35 +150,10 @@ const MyServicesContainer = ({
                     <a className={`nav-link ${tabsCurrent === DETAILS_TAB ? "active" : ""}`} onClick={() => AddQueryParam("serviceView", "DETAILS_TAB")}>Details</a>
                 </li>
             </ul>
-
-
-            {
-                tabsCurrent === OVERVIEW_TAB
-                && <ServiceOverview
-                    servicesList={provisionedServicesList}
-                    serviceIdSelected={serviceIdSelected}
-                    onSelectService={handleSelectService}
-                    onStartService={handleStartService}
-                    onStopService={handleStopService} />
-            }
-
-            {
-                tabsCurrent === DETAILS_TAB
-                && <ServiceDetails
-                    servicesList={provisionedServicesList}
-                    serviceIdSelected={serviceIdSelected}
-                    onSelectService={handleSelectService}
-                    onStartService={handleStartService}
-                    onStopService={handleStopService} />
-            }
         </div>
         {
             interfaceModeType === IMPORT_SELECT_MODE
             && <ImportRepositoryModal onImport={handleImportingMode} onClose={() => changeMode(DEFAULT_MODE)} />
-        }
-        {
-            interfaceModeType === SERVICE_PROVISIONING_MODE
-            && <ServiceProvisioningModal onClose={() => changeMode(DEFAULT_MODE)} />
         }
         {
             interfaceModeType === REPOSITORIES_MANAGER_MODE
