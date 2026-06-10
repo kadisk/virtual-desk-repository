5289f4c1e92703e9a15da86e971dbc1efb2e5b49
Kaio Cezar <kadisk.shark@gmail.com>
2026-06-10 00:55:02 -0300

    Copyright © 2025 KADISK

--- Platform.Module/PanelApplications.layer/UserSpacePanel.group/user-space-panel.webgui/src/PageComponents/WelcomeFooter.tsx
+++ Platform.Module/PanelApplications.layer/UserSpacePanel.group/user-space-panel.webgui/src/PageComponents/WelcomeFooter.tsx
@@ -7,7 +7,7 @@ const WelcomeFooter = () => {
                         <div className="col-12 col-lg-auto mt-3 mt-lg-0">
                             <ul className="list-inline list-inline-dots mb-0">
                                 <li className="list-inline-item">
-                                    Copyright © 2025 KADISK Engenharia de Software Ltda. Todos os direitos reservados.
+                                    Copyright © 2026 KADISK Engenharia de Software Ltda. Todos os direitos reservados.
                                 </li>
                                 <li className="list-inline-item">
                                     CNPJ: 50.706.455/0001-03
